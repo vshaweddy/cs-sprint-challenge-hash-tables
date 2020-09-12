fe51036c1901a7039b6084ed5f147687b2efc82c
Vici Shaweddy <vshaweddy@gmail.com>
2020-09-11 22:50:59 -0400

    second problem solved

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,27 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+
+    # create a look up table where the key is the start and the destination is the value
+    lookup_table = {}
+    for ticket in tickets:
+        lookup_table[ticket.source] = ticket.destination
+
+    # start with key that is none and put the value into the list using the loop
+    current = "NONE"
+
+    # create a route array
+    route = []
+
+    while current:
+        value = lookup_table[current]
+        route.append(value)
+
+        current = value
+        # stop until the value is none
+        if value == "NONE":
+            break
+
+    # return an array of the destinations
     return route
